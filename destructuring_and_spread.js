1a08757ca55c357d8588ad8a23995b963fd96fe8
Rodgers Ogada <ogadarodgers@gmail.com>
2025-03-12 14:05:07 +0300

    spread operator

--- destructuring_and_spread.js
+++ destructuring_and_spread.js
@@ -28,12 +28,76 @@ const [zero, one, two] = colors;
 // console.log(one);
 // console.log(two);
 
-// 3. strings 
+// 3. strings
 // --> order is important
-// --> 
+// --> do a split method
+// --> returns an array which can be destructured.
 const yourName = 'Hon Donald Trump'; // title, firstName, secondName
 const [title, firstName, secondName] = yourName.split(' ');
 // console.log(title);
 
 const splitting = yourName.split(' ')[0];
-console.log(splitting);
+// console.log(splitting);
+
+// SPREAD OPERATORS
+// --> merge, update or copy data in arrays and objects
+
+// syntax (...)
+// 1. working with arrays [...]
+// a. copy an array
+const numbers = [1, 2, 3, 4, 5];
+const copiedNumbers = [...numbers];
+// [1, 2, 3, 4, 5]
+// console.log(numbers)
+// console.log(copiedNumbers)
+
+// b. merge arrays
+// merge numbers[] and copiedNumbers[] to create mergedArr[]
+const mergedArr = [...numbers, ...copiedNumbers];
+// console.log(mergedArr)
+
+// merge arr1[] and arr2[] to create merged[]
+// [1, 2, 3, 4, 5, 6, 7, 8]
+const arr1 = [1, 2, 3, 4];
+const arr2 = [5, 6, 7, 8];
+const merged = [...arr1, ...arr2];
+// console.log(merged);
+
+// c. adding an element to an array
+// add 0,6,7 to our numbers[1, 2, 3, 4, 5] --> [0,1,2,3,4,5,6,7]
+// [0,1,2,3,4,5,6,7]
+const addElements = [0, ...numbers, 6, 7]; // [...numbers, 0, 8, 10, 15] --> [1,2,3,4,5, 0, 8, 10, 15]
+// console.log(addElements, numbers);
+
+// 2. working with objects
+// a. copy an object
+// const student = {
+//   name: 'Ken',
+//   course: 'software dev',
+//   age: 18,
+//   isActive: true,
+//   hobbies: ['coding', 'gaming', 'football', 'dancing'],
+//   phoneNumber: '+254701234567',
+// };
+
+const copiedStudent = { ...student };
+// const sqr = [student];
+// console.log(sqr);
+// console.log(copiedStudent);
+
+// b. merging objects
+// merging user{} and details{} to create userDetails that has data from both objects
+const user = { name: 'vincent', age: 30 };
+const details = { country: 'kenya', job: 'dev' };
+const contacts = { email: 'test@example.com', phoneNumber: '+254701234567' };
+const userDetails = { ...user, ...contacts, ...details }; // { name: 'vincent', age: 30, country: 'kenya', job: 'dev' }
+// console.log(userDetails);
+
+// updating data in an object
+const updatedUser = {
+  ...userDetails,
+  age: 20,
+  email: 'vincent@mail.org',
+  linkedIn: 'linkedin/vincent-job',
+};
+console.log(updatedUser);
